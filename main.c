db58a5e4f8a584bcecc3dae9313a65fb79e643d7
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-14 12:52:50 +0000

    making the base

--- main.c
+++ main.c
@@ -7,5 +7,33 @@
  */
 int main(int argc, char *argv[])
 {
-    return (EXIT_FAILURE);
+FILE* file;
+char *buffer = NULL;
+size_t buffer_num = 0;
+size_t line = 1;
+
+
+
+if (argc != 2) 
+{
+	dprintf(STDERR_FILENO, "USAGE: monty file");
+	exit(EXIT_FAILURE);
+}
+
+file = fopen(argv[1], "r")
+if (file == NULL)
+{
+	dprintf(STDERR_FILENO, "USAGE: monty file");
+	exit(EXIT_FAILURE);
+}
+
+
+for (line = 1, getline(&buffer, &buffer_line, file) != 0, line++)
+{
+dprintf(STDOUT_FILENO, "%s",buffer);
+free(buffer);
+}
+
+fclose(file);
+return (0);
 }
