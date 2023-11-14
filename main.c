e099282fcab651c17eef50c048cce6429a476613
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-14 12:35:56 +0000

    trying

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * main - This program is to create a stack using a monty file as input
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: EXIT_FAILURE in any occured issue, else 0
+ */
+int main(int argc, char *argv[])
+{
+    return (EXIT_FAILURE);
+}
